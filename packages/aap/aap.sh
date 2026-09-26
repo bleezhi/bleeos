5ed42eeb864d3e8da054a0b68fe642d444e1fce9
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-26 18:12:22 +0200

    apps: make aap an ASCII animation program

---
+++ packages/aap/aap.sh
@@ -0,0 +1,85 @@
+# aap: ASCII animation player for BleeOS
+# Run with: run /pkg/aap/aap.sh
+
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "          o\n"
+printf "         /|\\\n"
+printf "         / \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "         \\o/\n"
+printf "          |\n"
+printf "         / \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "           o\n"
+printf "          /|\\\n"
+printf "          / \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "         __o\n"
+printf "          /\\\n"
+printf "         /  \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "        _o__\n"
+printf "         /\\\n"
+printf "        /  \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "           o\n"
+printf "          /|\\\n"
+printf "          / \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "         \\o/\n"
+printf "          |\n"
+printf "         / \\\n"
+sleep 1
+clear
+printf "        .----.\n"
+printf "       /      \\\n"
+printf "      |  AAP!  |\n"
+printf "       \\      /\n"
+printf "        '----'\n"
+printf "          o\n"
+printf "         /|\\\n"
+printf "         / \\\n"
+sleep 1
+clear
+printf "aap: animation complete\n"
