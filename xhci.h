54d59e04e5dbb88bb51bfebde538952ebe59b237
blee <95976420+bleezhi@users.noreply.github.com>
2026-09-26 18:29:18 +0200

    usb: add xHCI driver interface

---
+++ xhci.h
@@ -0,0 +1,10 @@
+#ifndef XHCI_H
+#define XHCI_H
+int xhci_init(void);
+int xhci_present(void);
+int xhci_nports(void);
+int xhci_connected(int port);
+int xhci_enumerate_port(int port,int index);
+int xhci_hid_trykey(int index,int *out);
+int xhci_hid_mouse(int index,int *dx,int *dy,int *btn);
+#endif
